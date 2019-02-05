e8c252f7ce0999da68ad3d8eaa086c6f44a7e2f6
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-02-05 18:26:43 +0300

    Добавлен скрипт для разделения файлов на выборки
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

---
+++ train_test_split.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    test_ratio = float(sys.argv[1])
+    files = sys.argv[2:]
+    for fname in files:
+        if not os.path.exists(fname):
+            print("File {} not found".format(fname))
+            exit()
+        fname_split = os.path.split(fname)[-1]
+        train_path = os.path.join(os.path.dirname(fname),
+                                  "train_" + fname_split)
+        test_path = os.path.join(os.path.dirname(fname),
+                                 "test_" + fname_split)
+        with open(train_path, "w") as train_file, open(test_path, "w") as test_file:
+            write_header = True
+            for num, chunk in enumerate(pd.read_csv(fname)):
+                chunk_size = len(chunk.index)
+                train_rows_num = int(chunk_size * (1 - test_ratio))
+                train_chunk = chunk.iloc[:train_rows_num]
+                train_chunk.to_csv(train_file,
+                                   sep="\t",
+                                   index=None,
+                                   header=write_header)
+                test_chunk = chunk.iloc[train_rows_num + 1:]
+                test_chunk.to_csv(test_file,
+                                  sep="\t",
+                                  index=None,
+                                  header=write_header)
+                write_header = False
