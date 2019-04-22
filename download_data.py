4de57417bb87dc989769b3d84f6fb5361dd9aead
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-04-22 14:46:46 +0300

    Добавлен код для загрузки новых данных
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- download_data.py
+++ download_data.py
@@ -1,6 +1,14 @@
 import urllib.request
 
-urllib.request.urlretrieve("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Movies_and_TV_5.json.gz", 
-                  "../data/Movies_and_TV_5.json.gz")
-urllib.request.urlretrieve("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Electronics_5.json.gz", 
-                  "../data/Electronics_5.json.gz")
+urllib.request.urlretrieve("http://snap.stanford.edu/"
+                           "data/amazon/productGraph/"
+                           "categoryFiles/reviews_CDs_and_Vinyl_5.json.gz",
+                           "../data/CDs_and_Vinyl_5.json.gz")
+urllib.request.urlretrieve("http://snap.stanford.edu/"
+                           "data/amazon/productGraph/"
+                           "categoryFiles/reviews_Kindle_Store_5.json.gz",
+                           "Kindle_Store_5.json.gz")
+urllib.request.urlretrieve("http://snap.stanford.edu/"
+                           "data/amazon/productGraph/"
+                           "categoryFiles/reviews_Apps_for_Android_5.json.gz",
+                           "../data/Apps_for_Android_5.json.gz")
