4a7c6a1126e7c685c1e1ea69d4b9ac69dfe1015c
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-05-08 19:31:08 +0300

    Добавлено дообучение w2v
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- train_w2v.py
+++ train_w2v.py
@@ -1,3 +1,5 @@
+import sys
+
 from gensim.models import Word2Vec
 
 from utils2 import pp_chunk_generator
@@ -19,14 +21,26 @@ def create_w2v_model(files: list, batch_size=1000, vector_size=128) -> Word2Vec:
 
 
 if __name__ == '__main__':
-    files = [
-        "Apps_for_Android_5.json.gz",
-        "CDs_and_Vinyl_5.json.gz",
-        "Kindle_Store_5.json.gz",
-        "Electronics_5.json.gz",
-        "Movies_and_TV_5.json.gz",
-    ]
-    for i in range(len(files)):
-        files[i] = "../data/" + files[i]    # type: list
-    model = create_w2v_model(files, batch_size=20)
-    model.save("../models/w2v_5dom_v1.model")
+    if len(sys.argv) == 1:
+        files = [
+            "Apps_for_Android_5.json.gz",
+            "CDs_and_Vinyl_5.json.gz",
+            "Kindle_Store_5.json.gz",
+            "Electronics_5.json.gz",
+            "Movies_and_TV_5.json.gz",
+        ]
+        for i in range(len(files)):
+            files[i] = "../data/" + files[i]    # type: list
+        model = create_w2v_model(files, batch_size=20)
+        model.save("../models/w2v_5dom_v1.model")
+    else:
+        model_fp = sys.argv[1]
+        train_file = sys.argv[2]
+        out_file = sys.argv[3]
+        w2v = Word2Vec.load(model_fp)
+        gen = pp_chunk_generator(train_file, 50)
+        for batch in gen:
+            sentence = [text.split() for text in batch["reviewText"].values]
+            w2v.build_vocab(sentence, update=True)
+            w2v.train(sentence, epochs=20, total_examples=len(sentence))
+        w2v.save(out_file)
