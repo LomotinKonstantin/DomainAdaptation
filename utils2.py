59b9a2bafecfeff4f8d921e4783bfc9f1629b3e2
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-05-14 21:45:13 +0300

    Добавлен генератор векторов
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- utils2.py
+++ utils2.py
@@ -2,7 +2,9 @@ from collections import OrderedDict
 import gzip
 import json
 
+import numpy as np
 import pandas as pd
+from gensim.models import Word2Vec
 
 from preprocessor import Preprocessor
 
@@ -22,7 +24,8 @@ def raw_chunk_generator(path: str,
                 return
             parsed_line = json.loads(json_line, object_pairs_hook=OrderedDict)
             parsed_line["reviewText"] += " " + parsed_line["summary"]
-            parsed_line = {key: value for key, value in parsed_line.items() if key in columns}
+            parsed_line = {key: value for key, value in parsed_line.items()
+                           if key in columns}
             chunk.append(parsed_line)
             if len(chunk) == chunk_size:
                 yield pd.DataFrame(chunk)
@@ -31,25 +34,38 @@ def raw_chunk_generator(path: str,
         yield pd.DataFrame(chunk)
 
 
-def pp_chunk_generator(path: str,
-                       chunk_size: int,
-                       from_line: int = 0,
-                       to_line: int = 0) -> pd.DataFrame:
+def vector_chunk_generator(path: str,
+                           chunk_size: int,
+                           w2v_model: Word2Vec,
+                           from_line: int = 0,
+                           to_line: int = 0,
+                           ) -> pd.DataFrame:
     preprocessor = Preprocessor()
     for chunk in raw_chunk_generator(path,
                                      chunk_size,
                                      from_line=from_line,
                                      to_line=to_line):
-        chunk["vector"] = chunk["reviewText"].apply(preprocessor.preprocess)
+        chunk["vectors"] = text_to_matr(
+            chunk["reviewText"].apply(preprocessor.preprocess),
+            w2v_model
+        )
         yield chunk
 
 
-def text_to_matr(text: str):
-    return text
+def text_to_matr(text: str, w2v_model) -> np.array:
+    tokens = text.split()
+    vec_lst = []
+    for word in tokens:
+        try:
+            vec_lst.append(np.array(w2v_model.wv.get_vector(word)))
+        except KeyError:
+            pass
+    return np.array(vec_lst)
 
 
 if __name__ == '__main__':
-    gen = pp_chunk_generator("../data/reviews_Musical_Instruments_5.json.gz",
-                             chunk_size=10, to_line=10, from_line=100)
+    w2v = Word2Vec.load("../models/w2v_5dom.model")
+    gen = vector_chunk_generator("../data/reviews_Musical_Instruments_5.json.gz",
+                                 chunk_size=10, to_line=10, from_line=100, w2v_model=w2v)
     df = next(gen)
     print(df)
