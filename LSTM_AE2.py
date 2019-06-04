dbc25d527e80525ab0c1f74432cd55ac30680f49
Konstantin Lomotin <ke.lomotin@gmail.com>
2019-06-04 20:56:11 +0300

    debug ae #1
    Signed-off-by: Konstantin Lomotin <ke.lomotin@gmail.com>

--- LSTM_AE2.py
+++ LSTM_AE2.py
@@ -4,23 +4,25 @@ from all_models_source_target import *
 if __name__ == '__main__':
     timestamp = get_timestamp()
     data_folder = "../data/"
-    report_folder = f"../reports/AE_{timestamp}/"
-    model_folder = f"../models/AE_{timestamp}/"
-    os.mkdir(report_folder)
-    os.mkdir(model_folder)
+    # report_folder = f"../reports/AE_{timestamp}/"
+    # model_folder = f"../models/AE_{timestamp}/"
+    report_folder = "../reports/28-May-2019__03-06-35/"
+    model_folder = "../models/28-May-2019__03-06-35/"
+    # os.mkdir(report_folder)
+    # os.mkdir(model_folder)
 
-    model = create_AE_model(64, 128)
-    print("Training AE layers")
-    train_model(model, train_files=train_files, batch_size=batch_size,
-                epochs=epochs, ae=True, line_count_hint=line_counts,
-                test_percent=test_percent, w2v_model=w2v_model)
-    model.layers.pop()
-    model = create_lstm_classifier(model)
+    # model = create_AE_model(64, 128)
+    # print("Training AE layers")
+    # train_model(model, train_files=train_files, batch_size=batch_size,
+    #             epochs=epochs, ae=True, line_count_hint=line_counts,
+    #             test_percent=test_percent, w2v_model=w2v_model)
+    # model.layers.pop()
+    # model = create_lstm_classifier(model)
     clear_path = model_folder + "AE_LSTM_clear.hdf5"
-    model.save(clear_path)
-    print("Training AE+LSTM on source")
-    train_on_source(model, False)
-    model.save(model_folder + "AE_LSTM_source.hdf5")
+    # model.save(clear_path)
+    # print("Training AE+LSTM on source")
+    # train_on_source(model, False)
+    model = load_model(model_folder + "AE_LSTM_source.hdf5")
     print("Testing AE+LSTM on source")
     test_on_source(model, report_folder + "AE_LSTM_source-source.csv")
     print("Testing AE+LSTM on target")
